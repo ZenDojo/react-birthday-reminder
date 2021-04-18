54e3ffa7ccae3d7e4bff4f9dffdecb23b2ab35f6
ZenDojo <68330321+ZenDojo@users.noreply.github.com>
2021-04-18 08:33:29 -0700

    new

---
+++ precache-manifest.797a232253a35ff2ec7a6faf593d4b2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99529bec6c7689a3a85e5e041f4d5f5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bf3ed1bd773d70373f78",
+    "url": "./static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "./static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf3ed1bd773d70373f78",
+    "url": "./static/js/main.968b0d03.chunk.js"
+  },
+  {
+    "revision": "634688380851e0148535",
+    "url": "./static/js/runtime-main.9a51be4d.js"
+  }
+]);
